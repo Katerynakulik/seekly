e633ecd56cd8ed7ddf4081401d57a5836cfd603f
Katerynakulik <katerynakulik4@gmail.com>
2025-09-24 22:34:46 +0300

    Create a function that creates an HTML structure for image search

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 const BASE_URL = "https://pixabay.com/api/";
 export async function getImagesByQuery(query, page) {
   const searchParams = {
@@ -17,3 +19,53 @@ export async function getImagesByQuery(query, page) {
     console.error("Error fetching images:", error);
   }
 }
+const container = document.querySelector(".gallery");
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
+
+export function createGallery(images) {
+  function itemInsert({
+    webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads,
+  }) {
+    return `<li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
+        </a>
+        <div class="image-describe">
+        <div class="img-info">
+          <b>Likes</b>
+          <p>${likes}</p>
+        </div>
+        <div class="img-info">
+          <b>Views</b>
+          <p>${views}</p>
+        </div>
+        <div class="img-info">
+          <b>Comments</b>
+          <p>${comments}</p>
+        </div>
+        <div class="img-info">
+          <b>Downloads</b>
+          <p>${downloads}</p>
+        </div>
+        </div>
+      </li>
+    `;
+  }
+  const newGallery = images.map(itemInsert).join("");
+  container.insertAdjacentHTML("beforeend", newGallery);
+
+  lightbox.refresh();
+}
+
+console.log("mygallery", container);
