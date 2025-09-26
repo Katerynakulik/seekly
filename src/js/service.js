1c44d0c6b82cf9572123d3fdd55c793482ee71a6
Katerynakulik <katerynakulik4@gmail.com>
2025-09-26 12:43:55 +0300

    Delete unnecessery tags and functions

--- src/js/service.js
+++ src/js/service.js
@@ -1,9 +1,3 @@
-export function showLoadMoreButton() {
-  document.querySelector(".load-more").classList.remove("hidden");
-}
-export function hideLoadMoreButton() {
-  document.querySelector(".load-more").classList.add("hidden");
-}
 // Show loader
 export const showLoader = () => {
   document.querySelector("main .base-image").classList.add("hidden");
@@ -15,23 +9,8 @@ export const hideLoader = () => {
   document.querySelector(".loader").classList.add("hidden");
   document.querySelector(".results").classList.remove("hidden");
 };
-// Reset gallery before new search
-export const resetGallery = () => {
-  document.querySelector(".gallery").innerHTML = "";
-  hideLoadMoreButton();
-};
+
 // Clear gallery
 export function clearGallery() {
   document.querySelector(".gallery").innerHTML = "";
-}
-// Scroll after loading
-export function scrollPage() {
-  const card = document.querySelector(".gallery-item");
-  if (!card) return;
-
-  const cardHeight = card.getBoundingClientRect().height;
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: "smooth",
-  });
 }
--- src/main.js
+++ src/main.js
@@ -1,24 +1,14 @@
 import { fetchMovies, renderMovieGrid } from "./js/fetchMovies.js";
 import { getImagesByQuery, createGallery } from "./js/fetchImages.js";
-import {
-  showLoadMoreButton,
-  hideLoadMoreButton,
-  showLoader,
-  hideLoader,
-  resetGallery,
-  clearGallery,
-  scrollPage,
-} from "./js/service.js";
+import { showLoader, hideLoader, clearGallery } from "./js/service.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const searchForm = document.getElementById("search-form");
   const queryInput = searchForm.querySelector('input[name="query"]');
-  const loadMoreBtn = document.querySelector(".load-more");
+  const baseImage = document.querySelector(".base-image");
 
   let currentQuery = "";
   let currentCategory = "";
-  let currentPage = 1;
-  let totalHits = 0;
 
   // Handle movie card click
   const handleMovieSelect = (movie) => {
@@ -45,17 +35,17 @@ document.addEventListener("DOMContentLoaded", () => {
       });
       return;
     }
-    resetGallery();
+    clearGallery();
     showLoader();
-    hideLoadMoreButton();
+    currentQuery = query;
+    currentCategory = category;
 
     try {
       if (category === "movies") {
-        const movies = await fetchMovies(query, currentPage);
+        const movies = await fetchMovies(query, 1);
         if (!movies || movies.length === 0) {
-          hideLoadMoreButton();
           document.querySelector(".results").classList.add("hidden");
-          document.querySelector(".base-image").classList.remove("hidden");
+          baseImage.classList.remove("hidden");
 
           iziToast.warning({
             title: "No results",
@@ -66,14 +56,11 @@ document.addEventListener("DOMContentLoaded", () => {
           return;
         }
         renderMovieGrid(movies, handleMovieSelect);
-
-        if (movies.length > 0) showLoadMoreButton();
       } else if (category === "images") {
-        const images = await getImagesByQuery(query, currentPage);
+        const images = await getImagesByQuery(query, 1);
         if (!images || !images.hits || images.hits.length === 0) {
-          hideLoadMoreButton();
           document.querySelector(".results").classList.add("hidden");
-          document.querySelector(".base-image").classList.remove("hidden");
+          baseImage.classList.remove("hidden");
 
           iziToast.warning({
             title: "No results",
@@ -83,60 +70,13 @@ document.addEventListener("DOMContentLoaded", () => {
           });
           return;
         }
-        totalHits = images.totalHits;
+
         createGallery(images.hits);
-        if (images.hits.length > 0 && totalHits > 15) {
-          showLoadMoreButton();
-        }
-      } else {
-        iziToast.warning({
-          title: "Attention",
-          message: `Search for category "${category}" not yet implemented.`,
-          position: "topCenter",
-          timeout: 3000,
-        });
       }
     } finally {
       hideLoader();
     }
 
     queryInput.value = "";
-  });
-
-  // Load more
-  loadMoreBtn.addEventListener("click", async () => {
-    currentPage += 1;
-    showLoader();
-
-    try {
-      if (currentCategory === "movies") {
-        const movies = await fetchMovies(currentQuery, currentPage);
-        renderMovieGrid(movies, handleMovieSelect);
-        if (movies.length === 0) hideLoadMoreButton();
-      } else if (currentCategory === "images") {
-        const data = await getImagesByQuery(currentQuery, currentPage);
-        createGallery(data.hits);
-        scrollPage();
-
-        if (currentPage * 15 >= totalHits) {
-          hideLoadMoreButton();
-        }
-      } else {
-        iziToast.info({
-          title: "Notice",
-          message: "Load more is not implemented for this category.",
-          position: "topRight",
-        });
-        hideLoadMoreButton();
-      }
-    } catch (error) {
-      iziToast.error({
-        title: "Error",
-        message: "There was an error, please try again later.",
-        position: "topRight",
-      });
-    } finally {
-      hideLoader();
-    }
   });
 });
